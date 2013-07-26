8e428e943c40a4bb0999967b95afedb66fa34447
unknown <Administrator@WIN-C2A73241OQ8.Aethnet.com>
2013-07-26 16:50:49 -0400

    Final UI Changes
    Needed to make the UI a bit better, side bar improved

--- linuxMain.php
+++ linuxMain.php
@@ -13,82 +13,148 @@
 <body>
 
 <div id="header">
-	<img src="contact.jpg" alt="Contact info" height="334" width="550">
+	<img src="user_admin.jpg" alt="Admin" height="200" width="200">
 </div>
 
 <div id="menu">
 	<?php 
 		error_reporting(0);
-		if (isset($_GET['id'])) {
-			session_start();
-			$_SESSION['username'] = "testUser";
-			if (isset($_SESSION['username'])) {
-				echo "Welcome ";
-				echo $_SESSION['username'];
-				echo '<a href="logout.php">(logout)</a><br>';
+		session_start();
+		
+		//$_SESSION['login'] = 1;
+		//$_SESSION['username'] = username;
+		//$_SESSION['permission'] = 2;
+		
+		if (isset($_SESSION['login'])) {
+			if (isset($_GET['id'])) {
+				session_start();
+				if (isset($_SESSION['username'])) {
+					echo "Welcome ";
+					echo $_SESSION['username'];
+					echo '<a href="logout.php">(logout)</a><br>';
+				}
+				$serverId = $_GET['id'];
+				echo "<a href=\"linuxMain.php\">";
+				echo 'Return to server selection</a><br>';
+				echo "<a href=\"linuxNetMain.php?id=$serverId\">";
+				echo 'Network<br></a>';
+				echo "<a href=\"linuxSSHMain.php?id=$serverId\">";
+				echo 'SSH<br></a>';
+				echo "<a href=\"linuxTopMain.php?id=$serverId\">";
+				echo 'TOP<br></a>';
 			}
-			$serverId = $_GET['id'];
-			echo "<a href=\"linuxMain.php\">";
-			echo 'Return to server selection</a><br>';
-			echo "<a href=\"linuxNetMain.php?id=$serverId\">";
-			echo 'Network<br></a>';
-			echo "<a href=\"linuxSSHMain.php?id=$serverId\">";
-			echo 'SSH<br></a>';
-			echo "<a href=\"linuxTopMain.php?id=$serverId\">";
-			echo 'TOP<br></a>';
+			else {
+				session_start();
+				if (isset($_SESSION['username'])) {
+					echo "Welcome ";
+					echo $_SESSION['username'];
+					echo '<a href="logout.php">(logout)</a><br>';
+				}
+				echo '<h3>Server Selection</h3>';
+				include_once "ServerManagement.php";
+				$serv = new ServerManagement();
+				$results = new mysqli_result();
+				$results = $serv->serverList();
+				
+				
+				
+				while ($row = mysqli_fetch_array($results)) {
+					if ($serv->hasServerCredentials($_SESSION['username'], $row['ServerID'])) {
+						$serverId = $row['ServerID'];
+						$serverName = $row['ServerName'];
+						$serverIp = $row['ServerIP'];
+						echo "<a href=\"linuxMain.php?id=$serverId\">";
+			  			echo "$serverName - $serverIp ";
+						echo "</a>";
+					}
+				}
+				echo '<h3>Control Panel</h3>';
+				echo '<a href="addServerCredentials.php">Add server credentials</a><br>';
+				if ($_SESSION['permission'] == 1) {
+					echo '<a href="addServer.php">Add a new server</a><br>';
+	        		echo '<a href="newUser.php">Add a new user</a><br>';
+	        		echo '<a href="removeUser.php">Remove a user</a><br>';
+	        	}
+			}
 		}
-		else {
-			session_start();
-			if (isset($_SESSION['username'])) {
-				echo "Welcome ";
-				echo $_SESSION['username'];
-				echo '<a href="logout.php">(logout)</a><br>';
-			}
-			echo '<h5>Server Selection</h5>';
-			include_once "ServerManagement.php";
-			$serv = new ServerManagement();
-			$results = new mysqli_result();
-			$results = $serv->serverList();
-			
-			
-			
-			while ($row = mysqli_fetch_array($results)) {
-				$serverId = $row['ServerID'];
-				$serverName = $row['ServerName'];
-				$serverIp = $row['ServerIP'];
-				echo "<a href=\"linuxMain.php?id=$serverId\">";
-	  				echo "$serverName - $serverIp ";
-				echo "</a>";
-			}
-		}
-	
 	?>
 
 </div>
-<div align="center" id = "content">
+<div align="left" id = "content">
         
 	<fieldset>
-    		<legend>Personal Information</legend>
+    		<legend>Server</legend>
     			<p>
     			<?php
     			include_once "SSH.php";
     			
     			$ssh = new SSH;
     			session_start();
-    			
     			if (isset($_SESSION['login'])) {
 					$username = $_SESSION['username'];
 					if (isset($_GET['id'])) {
 						$serverID = $_GET['id']; //Grabs the server's ID for use in SSH commands (identifies the server to send SSH requests too)
-
 						
 						//Code Block to add a command
-						$input = "uname -a"; //Command you would like to execute
+						$input = "hostname"; //Command you would like to execute
 						$sshOutput = $ssh->sshStart($serverID, $username, $input); //don't change this, this requests a function to execute ssh commands
-						echo "Version: $sshOutput"; //$sshOutput is the output of the command, this places the output into the html document
+						echo '<h2>Hostname</h2>';
+                                                echo '<hr width="50%" align="left">';						
+						echo "$sshOutput"; //$sshOutput is the output of the command, this places the output into the html document
+						echo "<br>"; //new line
 						echo "<br>"; //new line
 						//End of Code Block
+
+						//Code Block to add a command
+                                                $input = "uname"; //Command you would like to execute
+                                                $sshOutput = $ssh->sshStart($serverID, $username, $input); //don't change this, this requests a function to execute ssh commands
+						echo '<h2>Server Type</h2>';
+                                                echo '<hr width="50%" align="left">';
+                                                echo "$sshOutput"; //$sshOutput is the output of the command, this places the output into the html document
+                                                echo "<br>"; //new line
+                                                echo "<br>"; //new line
+                                                //End of Code Block
+
+						//Code Block to add a command
+                                                $input = "uptime"; //Command you would like to execute
+                                                $sshOutput = $ssh->sshStart($serverID, $username, $input); //don't change this, this requests a function to execute ssh commands
+                                                echo '<h2>Uptime</h2>';
+                                                echo '<hr width="50%" align="left">';
+						echo "$sshOutput"; //$sshOutput is the output of the command, this places the output into the html document
+						echo "<br>"; //new line
+                                                echo "<br>"; //new line
+                                                //End of Code Block
 						
+						 //Code Block to add a command
+                                                $input = "date"; //Command you would like to execute
+                                                $sshOutput = $ssh->sshStart($serverID, $username, $input); //don't change this, this requests a function to execute ssh commands
+                                                echo '<h2>Date</h2>';
+                                                echo '<hr width="50%" align="left">';
+                                                echo "$sshOutput"; //$sshOutput is the output of the command, this places the output into the html document
+                                                echo "<br>"; //new line
+                                                echo "<br>"; //new line
+                                                //End of Code Block
+
+						//Code Block to add a command
+                                                $input = "cat /proc/cpuinfo"; //Command you would like to execute
+                                                $sshOutput = nl2br($ssh->sshStart($serverID, $username, $input)); //don't change this, this requests a function to execute ssh commands
+						echo '<h2>CPU</h2>';
+						echo '<hr width="50%" align="left">';
+						echo "$sshOutput"; //$sshOutput is the output of the command, this places the output into the html document
+                                                echo "<br>"; //new line
+                                                echo "<br>"; //new line
+                                                //End of Code Block
+
+						//Code Block to add a command
+                                                $input = "free -m"; //Command you would like to execute
+                                                $sshOutput = nl2br($ssh->sshStart($serverID, $username, $input)); //don't change this, this requests a function to execute ssh commands
+                                                echo '<h2>Memory</h2>';
+                                                echo '<hr width="50%" align="left">';
+						echo "<br>";
+                                                echo "$sshOutput"; //$sshOutput is the output of the command, this places the output into the html document
+                                                echo "<br>"; //new line
+                                                echo "<br>"; //new line
+                                                //End of Code Block
 						
 					}
 					else {
@@ -98,22 +164,14 @@
 				else {
 					echo "You are not logged in!<br>";
 				}
-				
 
     			?>
     			
-    		
-
 			</p>
   	</fieldset>
-	<a href="addServer.php">Add a new server</a><br>
-	<a href="newUser.php">Add a new user</a><br>
-
 
 </div>	
 </body>
 </html>
 
 <!-- Nikolay Krivulin 1130530-->
-
-
--- linuxServiceMain.php
+++ linuxServiceMain.php
@@ -19,50 +19,64 @@
 <div id="menu">
 	<?php 
 		error_reporting(0);
-		if (isset($_GET['id'])) {
-			session_start();
-			if (isset($_SESSION['username'])) {
-				echo "Welcome ";
-				echo $_SESSION['username'];
-				echo '<a href="logout.php">(logout)</a><br>';
+		session_start();
+		
+		//$_SESSION['login'] = 1;
+		//$_SESSION['username'] = username;
+		//$_SESSION['permission'] = 2;
+		
+		if (isset($_SESSION['login'])) {
+			if (isset($_GET['id'])) {
+				session_start();
+				if (isset($_SESSION['username'])) {
+					echo "Welcome ";
+					echo $_SESSION['username'];
+					echo '<a href="logout.php">(logout)</a><br>';
+				}
+				$serverId = $_GET['id'];
+				echo "<a href=\"linuxMain.php\">";
+				echo 'Return to server selection</a><br>';
+				echo "<a href=\"linuxNetMain.php?id=$serverId\">";
+				echo 'Network<br></a>';
+				echo "<a href=\"linuxSSHMain.php?id=$serverId\">";
+				echo 'SSH<br></a>';
+				echo "<a href=\"linuxTopMain.php?id=$serverId\">";
+				echo 'TOP<br></a>';
 			}
-			
-			$serverId = $_GET['id'];
-			echo "<a href=\"linuxMain.php\">";
-			echo 'Return to server selection</a><br>';
-			echo "<a href=\"linuxNetMain.php?id=$serverId\">";
-			echo 'Network<br></a>';
-			echo "<a href=\"linuxSSHMain.php?id=$serverId\">";
-			echo 'SSH<br></a>';
-			echo "<a href=\"linuxTopMain.php?id=$serverId\">";
-			echo 'TOP<br></a>';
+			else {
+				session_start();
+				if (isset($_SESSION['username'])) {
+					echo "Welcome ";
+					echo $_SESSION['username'];
+					echo '<a href="logout.php">(logout)</a><br>';
+				}
+				echo '<h3>Server Selection</h3>';
+				include_once "ServerManagement.php";
+				$serv = new ServerManagement();
+				$results = new mysqli_result();
+				$results = $serv->serverList();
+				
+				
+				
+				while ($row = mysqli_fetch_array($results)) {
+					if ($serv->hasServerCredentials($_SESSION['username'], $row['ServerID'])) {
+						$serverId = $row['ServerID'];
+						$serverName = $row['ServerName'];
+						$serverIp = $row['ServerIP'];
+						echo "<a href=\"linuxMain.php?id=$serverId\">";
+			  			echo "$serverName - $serverIp ";
+						echo "</a>";
+					}
+				}
+				echo '<h3>Control Panel</h3>';
+				echo '<a href="addServerCredentials.php">Add server credentials</a><br>';
+				if ($_SESSION['permission'] == 1) {
+					echo '<a href="addServer.php">Add a new server</a><br>';
+	        		echo '<a href="newUser.php">Add a new user</a><br>';
+	        		echo '<a href="removeUser.php">Remove a user</a><br>';
+	        	}
+			}
 		}
-		else {
-			session_start();
-			if (isset($_SESSION['username'])) {
-				echo "Welcome ";
-				echo $_SESSION['username'];
-				echo '<a href="logout.php">(logout)</a><br>';
-			}
-			
-			echo '<h5>Server Selection</h5>';
-			include_once "ServerManagement.php";
-			$serv = new ServerManagement();
-			$results = new mysqli_result();
-			$results = $serv->serverList();
-			
-			
-			
-			while ($row = mysqli_fetch_array($results)) {
-				$serverId = $row['ServerID'];
-				$serverName = $row['ServerName'];
-				$serverIp = $row['ServerIP'];
-				echo "<a href=\"linuxMain.php?id=$serverId\">";
-	  				echo "$serverName - $serverIp ";
-				echo "</a>";
-			}
-		}
-	
 	?>
 
 </div>

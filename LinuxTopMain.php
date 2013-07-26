0e36188ca91b96e6a1e8344edbfd16369ce9481a
unknown <Administrator@WIN-C2A73241OQ8.Aethnet.com>
2013-07-26 16:53:09 -0400

    Modified sidebar to show all functions
    Required for easy navigation

--- LinuxTopMain.php
+++ LinuxTopMain.php
@@ -4,8 +4,6 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-
-
 <head>
 <link rel="stylesheet" type="text/css" href="style.css"/>
 <title>3IA3 feedback </title>
@@ -14,62 +12,77 @@
 <body>
 
 <div id="header">
-	<img src="contact.jpg" alt="Contact info" height="334" width="550">
+	<img src="user_admin.jpg" alt="Admin" height="200" width="200">
 </div>
 
 <div id="menu">
 	<?php 
 		error_reporting(0);
-		if (isset($_GET['id'])) {
-			session_start();
-			if (isset($_SESSION['username'])) {
-				echo "Welcome ";
-				echo $_SESSION['username'];
-				echo '<a href="logout.php">(logout)</a><br>';
+		session_start();
+		
+		//$_SESSION['login'] = 1;
+		//$_SESSION['username'] = username;
+		//$_SESSION['permission'] = 2;
+		
+		if (isset($_SESSION['login'])) {
+			if (isset($_GET['id'])) {
+				session_start();
+				if (isset($_SESSION['username'])) {
+					echo "Welcome ";
+					echo $_SESSION['username'];
+					echo '<a href="logout.php">(logout)</a><br>';
+				}
+				$serverId = $_GET['id'];
+				echo "<a href=\"linuxMain.php\">";
+				echo 'Return to server selection</a><br>';
+				echo "<a href=\"linuxNetMain.php?id=$serverId\">";
+				echo 'Network<br></a>';
+				echo "<a href=\"linuxSSHMain.php?id=$serverId\">";
+				echo 'SSH<br></a>';
+				echo "<a href=\"linuxTopMain.php?id=$serverId\">";
+				echo 'TOP<br></a>';
 			}
-			
-			$serverId = $_GET['id'];
-			echo "<a href=\"linuxMain.php\">";
-			echo 'Return to server selection</a><br>';
-			echo "<a href=\"linuxNetMain.php?id=$serverId\">";
-			echo 'Network<br></a>';
-			echo "<a href=\"linuxSSHMain.php?id=$serverId\">";
-			echo 'SSH<br></a>';
-			echo "<a href=\"linuxTopMain.php?id=$serverId\">";
-			echo 'TOP<br></a>';
+			else {
+				session_start();
+				if (isset($_SESSION['username'])) {
+					echo "Welcome ";
+					echo $_SESSION['username'];
+					echo '<a href="logout.php">(logout)</a><br>';
+				}
+				echo '<h3>Server Selection</h3>';
+				include_once "ServerManagement.php";
+				$serv = new ServerManagement();
+				$results = new mysqli_result();
+				$results = $serv->serverList();
+				
+				
+				
+				while ($row = mysqli_fetch_array($results)) {
+					if ($serv->hasServerCredentials($_SESSION['username'], $row['ServerID'])) {
+						$serverId = $row['ServerID'];
+						$serverName = $row['ServerName'];
+						$serverIp = $row['ServerIP'];
+						echo "<a href=\"linuxMain.php?id=$serverId\">";
+			  			echo "$serverName - $serverIp ";
+						echo "</a>";
+					}
+				}
+				echo '<h3>Control Panel</h3>';
+				echo '<a href="addServerCredentials.php">Add server credentials</a><br>';
+				if ($_SESSION['permission'] == 1) {
+					echo '<a href="addServer.php">Add a new server</a><br>';
+	        		echo '<a href="newUser.php">Add a new user</a><br>';
+	        		echo '<a href="removeUser.php">Remove a user</a><br>';
+	        	}
+			}
 		}
-		else {
-			session_start();
-			if (isset($_SESSION('username'))) {
-				echo "Welcome ";
-				echo $_SESSION('username');
-				echo '<a href="logout.php">(logout)</a><br>';
-			}
-			echo '<h5>Server Selection</h5>';
-			include_once "ServerManagement.php";
-			$serv = new ServerManagement();
-			$results = new mysqli_result();
-			$results = $serv->serverList();
-			
-			
-			
-			while ($row = mysqli_fetch_array($results)) {
-				$serverId = $row['ServerID'];
-				$serverName = $row['ServerName'];
-				$serverIp = $row['ServerIP'];
-				echo "<a href=\"linuxMain.php?id=$serverId\">";
-	  				echo "$serverName - $serverIp ";
-				echo "</a>";
-			}
-		}
-	
 	?>
 
 </div>
 <div align="center" id = "content">
         
 	<fieldset>
-    		<legend>Personal Information</legend>
+    		<legend>Server Resource Usage</legend>
     			<p>
     			<?php
     			include_once "SSH.php";

d332f7d695abede91098c401ca9b28ce17203ce2
unknown <Administrator@WIN-C2A73241OQ8.Aethnet.com>
2013-07-26 16:53:29 -0400

    Changed styling
    Improves UI to look better as a home page

--- loginForm.php
+++ loginForm.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
+
+<center><img src="user_admin.jpg" alt="Admin" height="300" width="300"></center>
+
 <head>
 <meta charset="ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="style.css"/>
 <META HTTP-EQUIV="Cache-Control" CONTENT="no-cache">
 <title>Login</title>
 </head>
 <body>
+	<center>
 	<form action="login.php" method="post">
 		Username: <input name="username" type="text" id="username"><br>
 		Password: <input name="password" type="password" id="password"><br>
@@ -22,5 +27,6 @@
 				}
 		?>
 	</form>
+	</center>
 </body>
 </html>
